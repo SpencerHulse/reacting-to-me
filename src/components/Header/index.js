349b21bbac9540ea08f7f0bfef4f93100524151b
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-18 17:19:25 -0400

    Basic outline for header and navbar.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,8 @@ import Navigation from "../Navigation";
 
 function Header() {
   return (
-    <div>
+    <div className="d-flex justify-content-between my-4">
+      <h2 className="fs-1 ms-4">Digital Portfolio</h2>
       <Navigation />
     </div>
   );
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,14 @@
 function Navigation() {
-  return;
+  return (
+    <nav className="d-flex align-items-end">
+      <ul className="d-flex nav-ul">
+        <li className="nav-item pe-5 fs-4">About Me</li>
+        <li className="nav-item pe-5 fs-4">My Work</li>
+        <li className="nav-item pe-5 fs-4">Contact Info</li>
+        <li className="nav-item pe-5 fs-4">Resume</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
