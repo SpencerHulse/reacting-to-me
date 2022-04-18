d10d4e9d868cf1683ddf32efd12ac0e7a6ab4078
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-18 18:35:39 -0400

    Moved footer/contact section from old portfolio to react.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,36 @@
 function Footer() {
-  return;
+  return (
+    <footer class="footer" id="contact">
+      <a href="mailto:hulse.spencer@gmail.com" class="footer-link">
+        hulse.spencer@gmail.com
+      </a>
+      <a href="tel:+14236537820" class="footer-link">
+        423-653-7820
+      </a>
+      <ul class="social-list">
+        <li class="social-list-item">
+          <a
+            class="social-list-link"
+            href="https://github.com/SpencerHulse"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+        </li>
+        <li class="social-list-item">
+          <a
+            class="social-list-link"
+            href="https://www.linkedin.com/in/spencer-hulse-987521223/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
