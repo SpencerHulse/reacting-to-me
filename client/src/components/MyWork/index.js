82c744775356ca4260b8b66ffb29cafebe359473
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-26 22:31:04 -0400

    Reworked the My Work component to be dry and easier to maintain.

--- client/src/components/MyWork/index.js
+++ client/src/components/MyWork/index.js
@@ -1,13 +1,4 @@
-import tenantively from "../../assets/tenantively.png";
-import newJobsYou from "../../assets/newjobsyou-screenshot.png";
-import sayWhatYouWill from "../../assets/say-what-you-will.png";
-import soOnThatNote from "../../assets/so-on-that-note.png";
-import weatherDashboard from "../../assets/weather-dashboard.png";
-import taskMasterPro from "../../assets/taskmaster-pro.png";
-import workDayScheduler from "../../assets/work-day-scheduler.png";
-import jsCodeQuiz from "../../assets/js-code-quiz.png";
-import alexanderTheGreat from "../../assets/alexander-the-great.png";
-import nsense from "../../assets/nsense.png";
+import workData from "./workData";
 
 function MyWork() {
   return (
@@ -20,275 +11,44 @@ function MyWork() {
 
         <div className="portfolio">
           {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={nsense} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://n-sense.herokuapp.com/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    E-commerce
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/n-sense"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>React / MongoDB / Tailwind</span>
+          {workData.map((project, i) => {
+            const { projectName, imageSrc, deployedLink, repoLink, mainTech } =
+              project;
+            console.log(projectName);
+
+            return (
+              <div className="project-container" key={i}>
+                <div className="portfolio-container">
+                  <img
+                    src={require(`../../assets/${imageSrc}`)}
+                    alt=""
+                    className="portfolio-img"
+                  />
+                  <div className="portfolio-info">
+                    <p className="portfolio-item-links">
+                      <a
+                        href={deployedLink}
+                        className="portfolio-link"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {projectName}
+                      </a>
+                      <a
+                        className="repository-link"
+                        href={repoLink}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <i className="fa-brands fa-github github-portfolio-link"></i>
+                      </a>
+                    </p>
+                    <span>{mainTech}</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={tenantively} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://glacial-temple-16968.herokuapp.com/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Property Listings
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/tenantively-maybe-probably"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>Node / AWS s3 Bucket / Sequelize</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={newJobsYou} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://spencerhulse.github.io/newJobs-you/landingpage.html"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Job Aggregator
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/tenantively-maybe-probably"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript / jQuery / Materialize</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={sayWhatYouWill} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://quiet-caverns-00317.herokuapp.com/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Blog
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/say-what-you-will"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript / Node</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={soOnThatNote} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://mysterious-woodland-45975.herokuapp.com/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Note Taker
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/so-on-that-note"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript / Node</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={weatherDashboard} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://spencerhulse.github.io/the-weather-of-today/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Weather Dashboard
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/the-weather-of-today"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript / jQuery / Bootstrap</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={taskMasterPro} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://spencerhulse.github.io/taskmaster-pro/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Kanban Board
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/taskmaster-pro"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript / jQuery / Bootstrap</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={workDayScheduler} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://spencerhulse.github.io/the-only-work-day-scheduler-you-need/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Scheduler
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/the-only-work-day-scheduler-you-need"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript / jQuery / Bootstrap</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={jsCodeQuiz} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://spencerhulse.github.io/the-ultimate-js-code-quiz/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Quiz
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/the-ultimate-js-code-quiz"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript</span>
-              </div>
-            </div>
-          </div>
-          {/* portfolio item */}
-          <div className="project-container">
-            <div className="portfolio-container">
-              <img src={alexanderTheGreat} alt="" className="portfolio-img" />
-              <div className="portfolio-info">
-                <p className="portfolio-item-links">
-                  <a
-                    href="https://spencerhulse.github.io/alexander-tg-tribute/"
-                    className="portfolio-link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Tribute Page
-                  </a>
-                  <a
-                    className="repository-link"
-                    href="https://github.com/SpencerHulse/alexander-tg-tribute"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <i className="fa-brands fa-github github-portfolio-link"></i>
-                  </a>
-                </p>
-                <span>JavaScript</span>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </section>
     </div>
---
+++ client/src/components/MyWork/workData.js
@@ -0,0 +1,76 @@
+const workData = [
+  {
+    projectName: "E-commerce",
+    imageSrc: "nsense.png",
+    deployedLink: "https://n-sense.herokuapp.com/",
+    repoLink: "https://github.com/SpencerHulse/n-sense",
+    mainTech: "React / MongoDB / Tailwind",
+  },
+  {
+    projectName: "Property Listings",
+    imageSrc: "tenantively.png",
+    deployedLink: "https://glacial-temple-16968.herokuapp.com/",
+    repoLink: "https://github.com/SpencerHulse/tenantively-maybe-probably",
+    mainTech: "Node / AWS s3 Bucket / Sequelize",
+  },
+  {
+    projectName: "Job Aggregator",
+    imageSrc: "newjobsyou-screenshot.png",
+    deployedLink: "https://spencerhulse.github.io/newJobs-you/landingpage.html",
+    repoLink: "https://github.com/SpencerHulse/newJobs-you",
+    mainTech: "JavaScript / jQuery / Materialize",
+  },
+  {
+    projectName: "Blog",
+    imageSrc: "say-what-you-will.png",
+    deployedLink: "https://quiet-caverns-00317.herokuapp.com/",
+    repoLink: "https://github.com/SpencerHulse/say-what-you-will",
+    mainTech: "JavaScript / Node",
+  },
+  {
+    projectName: "Note Taker",
+    imageSrc: "so-on-that-note.png",
+    deployedLink: "https://mysterious-woodland-45975.herokuapp.com/",
+    repoLink: "https://github.com/SpencerHulse/so-on-that-note",
+    mainTech: "JavaScript / Node",
+  },
+  {
+    projectName: "Weather Dashboard",
+    imageSrc: "weather-dashboard.png",
+    deployedLink: "https://spencerhulse.github.io/the-weather-of-today/",
+    repoLink: "https://github.com/SpencerHulse/the-weather-of-today",
+    mainTech: "JavaScript / jQuery / Bootstrap",
+  },
+  {
+    projectName: "Kanban Board",
+    imageSrc: "taskmaster-pro.png",
+    deployedLink: "https://spencerhulse.github.io/taskmaster-pro/",
+    repoLink: "https://github.com/SpencerHulse/taskmaster-pro",
+    mainTech: "JavaScript / jQuery / Bootstrap",
+  },
+  {
+    projectName: "Scheduler",
+    imageSrc: "work-day-scheduler.png",
+    deployedLink:
+      "https://spencerhulse.github.io/the-only-work-day-scheduler-you-need/",
+    repoLink:
+      "https://github.com/SpencerHulse/the-only-work-day-scheduler-you-need",
+    mainTech: "JavaScript / jQuery / Bootstrap",
+  },
+  {
+    projectName: "Quiz",
+    imageSrc: "js-code-quiz.png",
+    deployedLink: "https://spencerhulse.github.io/the-ultimate-js-code-quiz/",
+    repoLink: "https://github.com/SpencerHulse/the-ultimate-js-code-quiz",
+    mainTech: "JavaScript",
+  },
+  {
+    projectName: "Tribute Page",
+    imageSrc: "alexander-the-great.png",
+    deployedLink: "https://spencerhulse.github.io/alexander-tg-tribute/",
+    repoLink: "https://github.com/SpencerHulse/alexander-tg-tribute",
+    mainTech: "JavaScript",
+  },
+];
+
+export default workData;
