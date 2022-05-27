0de72bea48cde09fe52e0c4f8c49180040e4e920
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-26 22:49:00 -0400

    Added production server code.

--- server/server.js
+++ server/server.js
@@ -38,5 +38,13 @@ app.post("/send_mail", cors(), async (req, res) => {
   });
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
